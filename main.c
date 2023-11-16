62d6bb079b2592109e8bc561ca6825a60d4a1c78
Samuel Henrique <samuelhenriquemoraisvitrio@gmail.com>
2023-11-16 01:40:23 -0200

    soma_de_caracter
    implantei algumas formas de somar os caracteres que colocar nos ponteiros da função.

--- main.c
+++ main.c
@@ -1,20 +1,48 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 //criei função com 2 ponteiros para as strings que irei utilizar
-void somaS(char *string1, const char *string2){
+void somas(char *string1, const char *string2){
+
 
     while(*string1 != '\0'){
         string1++;
     }
 
     while (*string2 != '\0'){
-        string1 = string2;
+        *string1 = *string2;
         string1++;
         string2++;
+    }
+    
+    *string1 = '\0';
+
+    size_t primeirosCaracterSemEspaco = strcspn(*string1, " ");
+
+    do{
+        string1 = 0;
+    }while(*string1 != '\0'){
+        if(*string1 != ''){
+            numComEspacos++;
+        }else{
+            numSemEspacos++;
+        }
+        
     }
 }
 
 int main(){
 
-    printf("\nola mundo\n");
+    short numSemEspacos;
+    short numComEspacos;
+
+    char st1[61];
+    printf("Escreva uma frase\n\n");
+
+    fgets(st1, sizeof(st1), stdin);
+
+
+    //system("cls");
+    //printf("\nSua frase foi '%s' \nCom %d caracters e %d tirando os espaços e com %d caracters antes do primeiro espaço.\n");
 }
