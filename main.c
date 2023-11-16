de39ed55dcbeb7a6abd087917d4c35d5b60349ec
Samuel Henrique <samuelhenriquemoraisvitrio@gmail.com>
2023-11-15 22:24:51 -0200

    Função da soma
    Criei uma função onde utilizando ponteiros eu poderei somar as string colocando as duas em uma unica frase.

--- main.c
+++ main.c
@@ -1,5 +1,20 @@
 #include <stdio.h>
 
-void main(){
-    
+//criei função com 2 ponteiros para as strings que irei utilizar
+void somaS(char *string1, const char *string2){
+
+    while(*string1 != '\0'){
+        string1++;
+    }
+
+    while (*string2 != '\0'){
+        string1 = string2;
+        string1++;
+        string2++;
+    }
+}
+
+int main(){
+
+    printf("\nola mundo\n");
 }
