a19a03831bc019023a4391bb12f05fa533985d19
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2023-11-22 20:26:49 -0300

    Finalizado-V1
    Criei um struct para que a função soma faça funções da bliblioteca string.h e agora a função retorna valores trocando assim o tipo da função de Void para Struct. O projeto esta finalmente funcionando corretamente porem à coisas simples para resolver e melhorar ainda.

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <string.h>
 
-int soma(char *st1, const char *st2){
+struct contagem{
+    short numCS;
+    short numSS;
+    short num;
+    short nunsAS;
+};
+
+struct contagem soma(char *st1, const char *st2){
     const char *st1A = st1;
     
     while(*st1 != '\0'){
@@ -15,6 +22,30 @@ int soma(char *st1, const char *st2){
     }
     
     *st1 = '\0';
+    
+    //return (st1 - st1A);
+    
+    st1 = st1A;
+    
+    struct contagem conta = {0, 0, 0, 0};
+    short primeiroS = 0;    
+    while(*st1 != '\0'){
+        if(*st1 != ' '){
+            conta.numSS++;
+            st1++;
+            
+            if(primeiroS == 0){
+                conta.nunsAS++;
+            }
+        }else{
+            conta.numCS++;
+            st1++;
+            primeiroS++;
+        }
+        conta.num++;
+    }
+    
+    return conta;
 }
 
 int main(){
@@ -22,15 +53,16 @@ int main(){
     char palavraR[51];
     const char fim[] = " mundo";
     
-    printf("****************");
+    printf("******");
     printf("\nescreva uma palavra com no max 50 caracters.\n");
-    printf("****************\n\n");
+    printf("******\n\n");
     
     fgets(palavraR, sizeof(palavraR), stdin);
     palavraR[strcspn(palavraR,  "\n")] = '\0';
     printf(" \n%s\n\n", palavraR);
-    soma(palavraR, fim);
+    struct contagem resultado = soma(palavraR, fim);
     
     printf("\n %s\n\n", palavraR);
-    //printf("\nSua frase foi '%s' \nCom %d caracters e %d tirando os espaços e com %d caracters antes do primeiro espaço.\n");
+    printf(" %d\n", resultado.nunsAS);
+    printf("\nSua frase foi '%s'.\nCom %d caracters.\nE %d caracters tirando os espaços.\nE com %d caracters antes do primeiro espaço.\n\n", palavraR, resultado.num, resultado.numSS, resultado.nunsAS);
 }
